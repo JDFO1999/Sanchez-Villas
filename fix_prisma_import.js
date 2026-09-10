b7e05b025cc93783c99960510f11d9990579b83c
Ing. Jose D Fernandez O <jdfo1999@gmail.com>
2026-09-10 15:51:45 -0400

    Fix import path for Prisma in routines actions

---
+++ fix_prisma_import.js
@@ -0,0 +1,7 @@
+﻿const fs = require('fs');
+let c = fs.readFileSync('app/actions/routines.ts', 'utf8');
+
+c = c.replace(/import prisma from "@\/lib\/prisma"/, 'import prisma from "@/lib/db"');
+
+fs.writeFileSync('app/actions/routines.ts', c, 'utf8');
+console.log("Fixed prisma import in routines.ts");
