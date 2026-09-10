356a6bf45a3761d2d40a4658a10863f3eb57dc37
Ing. Jose D Fernandez O <jdfo1999@gmail.com>
2026-09-10 13:35:41 -0400

    Fix trailing syntax errors in login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -144,7 +144,6 @@ export default function LoginPage() {
             </Link>
           </div>
 
-        </div>
       </div>
     </div>
   )
---
+++ fix_dangling_div.js
@@ -0,0 +1,15 @@
+﻿const fs = require('fs');
+let c = fs.readFileSync('app/login/page.tsx', 'utf8');
+
+c = c.replace(/C[^\w\s]*dula de Identidad/g, 'Cédula de Identidad');
+c = c.replace(/Contrase[^\w\s]*a/g, 'Contraseña');
+c = c.replace(/[^\w\s<]*Olvidaste tu clave\?/g, '¿Olvidaste tu clave?');
+c = c.replace(/Iniciar Sesi[^\w\s]*n/g, 'Iniciar Sesión');
+c = c.replace(/[^\w\s<]*Eres un nuevo atleta\?/g, '¿Eres un nuevo atleta?');
+c = c.replace(/Reg[^\w\s]*strate aqu[^\w\s]*/g, 'Regístrate aquí');
+
+// Also fix the extra </div>
+c = c.replace(/        <\/div>\r?\n      <\/div>\r?\n    <\/div>\r?\n  \)\r?\n}/, '      </div>\n    </div>\n  )\n}');
+
+fs.writeFileSync('app/login/page.tsx', c, 'utf8');
+console.log("Fixed dangling div and remaining mojibake");
